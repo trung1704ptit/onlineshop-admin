45029063cf5eb11f24c494a090dc7893518ed95b
Trung Nguyen <trung1704ptit@gmail.com>
2022-06-10 16:16:49 +0700

    init

---
+++ src/routes.ts
@@ -0,0 +1,39 @@
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+
+import { FC } from "react";
+
+// interface
+
+interface Route {
+  key: string;
+  title: string;
+  enabled: boolean;
+  component: FC<{}>;
+  path: string;
+}
+
+export const routes: Array<Route> = [
+  {
+    key: "homte-route",
+    title: "Home",
+    path: "/",
+    enabled: true,
+    component: Home,
+  },
+  {
+    key: "about-route",
+    title: "About",
+    path: "/about",
+    enabled: true,
+    component: About,
+  },
+  {
+    key: "products-route",
+    title: "Products",
+    path: "/products",
+    enabled: true,
+    component: Products,
+  },
+];
